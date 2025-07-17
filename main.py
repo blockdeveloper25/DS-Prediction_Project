ac9c265e915cef9ca7ad26f576025fda7ef9c67c
blockdeveloper25 <sujairblockdev@gmail.com>
2025-07-17 15:34:49 +0530

    logger file created

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.datascience import logger
+
+logger.info("This is a logger message from the __init__.py file of the datascience package.")
+logger.info("Logger has been set up successfully.")
